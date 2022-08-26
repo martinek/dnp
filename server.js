6d112b21f2713a4801960d6622b1583c875d08a3
Valter Martinek <martinek@freevision.sk>
2022-08-26 15:23:09 +0200

    Pisisvorove zmeny

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.on("connection", function (socket) {
   player.tell("Vitaj, napis si meno:")
 
   socket.on("data", function (chunk) {
-    const input = chunk.toString().trim()
+    let input = chunk.toString().trim()
 
     if (player.name === undefined) {
       player.name = input
@@ -22,22 +22,34 @@ server.on("connection", function (socket) {
       player.tell(`Ahoj ${player.name}`)
       player.look()
       console.log(`Player ${player.name} connected`)
+      player.location.notifyEntering(player)
       return
     }
 
-    const [command, ...args] = input.split(" ")
+    if (input === ".r") {
+      input = player.lastInput
+    }
+
+    let [command, ...args] = input.split(" ")
     console.log(`Player ${player.name}: ${command}`)
+    if (player.isDead) {
+      player.tell("Dead man tells no tales")
+      return
+    }
     switch (command) {
       case ".help":
-        player.tell(`=============\r
-Dostupne prikazy:\r
-  .help - tento help\r
-  .look - napise co vidi\r
-  .go - presunie ta do lokacie\r
-  .say - povie do lokacie\r
-  .yell - povie vsetkym hracom\r
-  .stats - ukaze tvoje statistiky\r
-  .challenge - vyzvi na suboj hraca\r
+        player.tell(`=============
+Dostupne prikazy:
+  .help - tento help
+  .look - napise co vidi
+  .go - presunie ta do lokacie
+  .say - povie do lokacie
+  .yell - povie vsetkym hracom
+  .stats - ukaze tvoje statistiky
+  .challenge - vyzvi na suboj hraca
+    .attack <num> - zautoci s hodnotou <num>
+    .buff - buffnes svoje sance na dalsi utok
+  .r - zopakuje posledny prikaz
 `)
         break
 
@@ -77,9 +89,15 @@ Dostupne prikazy:\r
         player.executeAttack(args[0])
         break
 
+      case ".buff":
+        player.executeBuff()
+        break
+
       default:
         break
     }
+
+    player.lastInput = input
   })
 
   socket.on("end", function () {
@@ -96,21 +114,24 @@ class Player {
     this.location = undefined
     this.health = rand(10, 20)
     this.attack = rand(2, 5)
+    this.buff = 1
+    this.ac = rand(1, 3)
+    this.lastInput = ""
   }
 
   tell(message) {
-    this._socket.write(message + "\r\n")
+    this._socket.write((message + "\n").replaceAll("\n", "\r\n"))
   }
 
   look() {
     this.tell(
-      `Lokacia "${this.location.name}"\r
-${this.location.description}\r
-Vies sa pohnut do: ${this.location.options.join(", ")}\r
+      `Lokacia "${this.location.name}"
+${this.location.description}
+Vies sa pohnut do: ${this.location.options.join(", ")}
 Vidim hracov: ${this.location
         .players()
         .map((p) => p.name)
-        .join(", ")}\r
+        .join(", ")}
 `
     )
   }
@@ -120,16 +141,10 @@ Vidim hracov: ${this.location
     if (newLocation === undefined) {
       this.tell(`Nepoznam ${newLocationName}`)
     } else if (this.location.options.includes(newLocation.name)) {
-      this.location.players().forEach((p) => {
-        if (p === this) return
-        p.tell(`${this.name} odisiel z miestnosti`)
-      })
+      this.location.notifyLeaving(this)
       this.location = newLocation
       this.look()
-      newLocation.players().forEach((p) => {
-        if (p === this) return
-        p.tell(`${this.name} vosiel do miestnosti`)
-      })
+      this.location.notifyEntering(this)
     } else {
       this.tell(`Neda sa!`)
     }
@@ -149,10 +164,13 @@ Vidim hracov: ${this.location
 
   stats() {
     this.tell(
-      `=============\r
-Statistiky:\r
-  health: ${this.health}\r
-  attack: ${this.attack}`
+      `========================
+Statistiky:
+  health: ${this.health}
+  ac:     ${this.ac}
+  attack: ${this.attack}
+  buff:   ${this.buff}
+========================`
     )
   }
 
@@ -205,25 +223,30 @@ Statistiky:\r
     if (isNaN(amount)) {
       this.tell("Kolko?")
       return
+    } else if (amount > 8) {
+      this.tell("Nie az tak silno")
+      return
     }
 
-    const target = 10 + amount
+    const target = 10 + amount + this.inCombatWith.ac
     const roll = rand(1, 21)
-    const totalRoll = roll + this.attack
+    const totalRoll = roll + this.attack * this.buff
     const isHit = totalRoll >= target
 
     if (!isHit) {
       this.combat.tellAll(
-        `Hrac ${this.name} netrafil (${totalRoll} / ${target})`
+        `Hrac ${this.name} netrafil (${roll} + ${this.attack} * ${this.buff}/ ${target} (${this.inCombatWith.ac}))`
       )
+      this.buff = 1
       this.combat.nextTurn()
       this.combat.reportTurn()
       return
     }
 
     this.combat.tellAll(
-      `Hrac ${this.name} trafil za ${amount} (${totalRoll} / ${target})`
+      `Hrac ${this.name} trafil za ${amount} (${roll} + ${this.attack} * ${this.buff}/ ${target} (${this.inCombatWith.ac}))`
     )
+    this.buff = 1
     this.inCombatWith.takeHit(amount)
     if (!this.inCombatWith.isDead) {
       this.combat.nextTurn()
@@ -232,11 +255,23 @@ Statistiky:\r
     }
 
     players.forEach((p) =>
-      p.tell(`============================================\r
-${this.name} zabil ${this.inCombatWith.name}\r
+      p.tell(`============================================
+${this.name} zabil ${this.inCombatWith.name}
 ============================================`)
     )
     this.combat.end()
+  }
+
+  executeBuff() {
+    if (!this.combat || this.combat.turn !== this) {
+      this.tell("Nie si na tahu / v combate")
+      return
+    }
+    this.buff += 0.5 //(*1.5)
+    this.combat.tellAll(`Hrac ${this.name} sa buffuje!(${this.buff})`)
+
+    this.combat.nextTurn()
+    this.combat.reportTurn()
   }
 
   takeHit(amount) {
@@ -258,6 +293,20 @@ class Location {
   players() {
     return players.filter((p) => p.location == this)
   }
+
+  notifyEntering(enteringPlayer) {
+    this.players().forEach((p) => {
+      if (p === enteringPlayer) return
+      p.tell(`${enteringPlayer.name} vosiel do miestnosti`)
+    })
+  }
+
+  notifyLeaving(leavingPlayer) {
+    this.players().forEach((p) => {
+      if (p === leavingPlayer) return
+      p.tell(`${leavingPlayer.name} odisiel z miestnosti`)
+    })
+  }
 }
 
 class Combat {
@@ -268,7 +317,9 @@ class Combat {
   }
 
   reportTurn() {
-    this.tellAll(`Na tahu je: ${this.turn.name}`)
+    this.tellAll(`
+========= Na tahu je: ${this.turn.name} =========
+`)
   }
 
   tellAll(message) {
